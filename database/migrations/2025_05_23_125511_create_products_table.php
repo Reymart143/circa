c7e8ebe0ba2497a2152143e114c6bbb3f79ad1bb
Reymart Roa <79199605+Reymart143@users.noreply.github.com>
2025-07-10 17:20:25 +0800

    Update 2025_05_23_125511_create_products_table.php

--- database/migrations/2025_05_23_125511_create_products_table.php
+++ database/migrations/2025_05_23_125511_create_products_table.php
@@ -14,15 +14,11 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_name')->nullable();
-            $table->time('end_time')->nullable();
             $table->double('price',10,2)->nullable(); 
-            $table->time('start_time')->nullable();
             $table->integer('status')->default(0);
             $table->string('category')->nullable();
              $table->string('image');
              $table->string('description')->nullable();
-             
-            $table->string('discount')->nullable();
             $table->timestamps();
         });
     }
